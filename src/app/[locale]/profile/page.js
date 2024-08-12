98487fb35cbf6703a1d3ad67e5dbdb1aef7e0458
hernan066 <hernan0066@gmail.com>
2024-08-12 14:12:05 -0300

    Add loading

--- src/app/[locale]/profile/page.js
+++ src/app/[locale]/profile/page.js
@@ -1,27 +1,14 @@
-import styles from "./page.module.css";
-import { Footer } from "@/components/footer/Footer";
-import { Navbar_index } from "@/components/navbar";
-import ConnectBtn from "@/components/profile/connectBtn/ConnectBtn";
-import MainProfile from "@/components/profile/main/Main";
 import Web3ModalProvider from "@/context/Web3ModalProvider";
+import { Navbar_index } from "@/components/navbar";
+import MainProfile from "@/components/profile/main/Main";
+import { Footer } from "@/components/footer/Footer";
 
 export default function Profile() {
   return (
     <Web3ModalProvider>
-      <main className={styles.main}>
-        <Navbar_index />
-        <div
-          style={{
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            height: 1000,
-          }}
-        >
-          <MainProfile />
-        </div>
-        <Footer />
-      </main>
+      <Navbar_index marquee={true} />
+      <MainProfile />
+      <Footer />
     </Web3ModalProvider>
   );
 }
--- src/components/profile/main/Main.js
+++ src/components/profile/main/Main.js
@@ -3,19 +3,20 @@ import Balance from "../balance/Balance";
 import { useAccount, useBalance, useReadContract } from "wagmi";
 import { ABI_TRC, TRC_CONTRACT } from "../../../../data/data_exchange";
 import { useFetch } from "@/hooks/useFetch";
+import styles from "./main.module.css";
 
 const MainProfile = () => {
-  const { isConnected, address } = useAccount();
-  const { data: priceTrcData } = useFetch(
+  const { address } = useAccount();
+  const { data: priceTrcData, loading: l1 } = useFetch(
     "0x34B08ccf9620aEd6d158BaE65e85Bb3bBe2c384A"
   );
-  const { data: priceBnbData } = useFetch(
+  const { data: priceBnbData, loading: l2 } = useFetch(
     "0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c"
   );
 
   const {
     data: balanceTrc,
-    isLoading: l1,
+    isLoading: l3,
     error,
   } = useReadContract({
     abi: ABI_TRC,
@@ -24,13 +25,18 @@ const MainProfile = () => {
     args: [address], //wallet, address
   });
 
-  const { data: balanceBnb, isLoading: l2 } = useBalance({
+  const { data: balanceBnb, isLoading: l4 } = useBalance({
     address,
   });
 
   return (
-    <div>
+    <section className={styles.container}>
       <w3m-button balance="false" size="sm" label="Conecta tu wallet" />
+      {l1 && l2 && l3 && l4 && (
+        <div className={styles.loading}>
+          <span class="loader"></span>
+        </div>
+      )}
       {address && balanceBnb && address && priceTrcData && priceBnbData && (
         <Balance
           data={{
@@ -42,7 +48,7 @@ const MainProfile = () => {
           priceBnbData={priceBnbData}
         />
       )}
-    </div>
+    </section>
   );
 };
 
