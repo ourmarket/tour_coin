4fca2c963fa2a864de25133dc8d4f9652787a06f
Our Market <148401898+ourmarket@users.noreply.github.com>
2025-02-19 17:34:49 -0300

    Remove section 8

--- src/app/[locale]/page.js
+++ src/app/[locale]/page.js
@@ -7,7 +7,7 @@ import { Section3_index } from "@/components/section3";
 import { Footer } from "@/components/footer/Footer";
 import { Navbar_index } from "@/components/navbar";
 import { Section7_index } from "@/components/section7";
-import { Section8_index } from "@/components/section8";
+
 import { Section9_index } from "@/components/section9";
 
 export default function Home() {
@@ -20,7 +20,7 @@ export default function Home() {
       <Section9_index />
       <Section4_index />
       <Section3_index />
-      <Section8_index />
+      
       <Section7_index />
       <Footer />
     </main>
