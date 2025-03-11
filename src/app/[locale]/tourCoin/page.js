76a0d021069d0db12e7de6cfae5f895a5f137aca
hernan066 <hernan0066@gmail.com>
2025-03-11 18:22:51 -0300

    Fix map

--- src/app/[locale]/tourCoin/page.js
+++ src/app/[locale]/tourCoin/page.js
@@ -1,12 +1,11 @@
 import { Navbar_index } from "@/components/navbar";
 import styles from "./page.module.css";
 import { Banner_index } from "@/components/banner";
-import { Section1_index } from "@/components/alliances/section1";
+import { Section1_index } from "@/components/tourCoin/section1";
 import { Section2_index } from "@/components/tourCoin/section2";
 import { Section9_index } from "@/components/tourCoin/section9";
 import { Section4_index } from "@/components/tourCoin/section4";
 import { Section3_index } from "@/components/tourCoin/section3";
-import { Section8_index } from "@/components/tourCoin/section8";
 import { Section7_index } from "@/components/tourCoin/section7";
 import { Footer } from "@/components/footer/Footer";
 
@@ -20,7 +19,6 @@ export default function Home() {
       <Section9_index />
       <Section4_index />
       <Section3_index />
-      <Section8_index />
       <Section7_index />
       <Footer />
     </main>
